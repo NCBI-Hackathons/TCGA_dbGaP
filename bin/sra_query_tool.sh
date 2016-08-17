54f6cf42c07b750575082894840b352677a49416
AbhijitBadve <badveabhijit@gmail.com>
2016-08-17 20:31:50 +0000

    Changed shell script.

--- bin/sra_query_tool.sh
+++ bin/sra_query_tool.sh
@@ -7,7 +7,7 @@ results=~/ewagner/sratoolkit.2.7.0-centos_linux64/HACKATHON2016
 input=~/ewagner/sratoolkit.2.7.0-centos_linux64/HACKATHON2016/SRRlist.txt
 
 #gene region to search
-genregion=4:1723150-1810650
+#genregion=4:1723150-1810650
 
 #can input multiple regions in the regionlist.txt file to query mutiple regions
 #region=~/ewagner/sratoolkit.2.7.0-centos_linux64/HACKATHON2016/regionlist.txt
@@ -15,6 +15,7 @@ genregion=4:1723150-1810650
 #alternative gene region to search
 #genregion=7:55086678-55279262
 
+genregion=$1
 
 while IFS= read -r sample;
 do
