c861b38d1c86572e2dd60f9a7caf17ac0c96b7c8
Satyajeet Raje <satyajeet.raje@gmail.com>
2016-08-27 14:29:12 -0400

    adding relative paths to "sra_query_tool.sh"

--- bin/sra_query_tool.sh
+++ bin/sra_query_tool.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-SRA=~/ewagner/sratoolkit.2.7.0-centos_linux64/bin
-results=~/ewagner/sratoolkit.2.7.0-centos_linux64/HACKATHON2016
-
-#input file with list of SRR IDs
-input=~/ewagner/sratoolkit.2.7.0-centos_linux64/HACKATHON2016/SRRlist.txt
+base= ./sratoolkit.2.7.0-centos_linux64 #path to the sra toolkit folder <default assumes current folder>
+#path to input file with list of SRR IDs
+input= ./SRRlist.txt #add the inout path here <default is the current folder>
+#path to output folder
+results= ./output #add the output path here <default is the current folder>
 
 #gene region to search
 #genregion=4:1723150-1810650
-
+SRA=base + /bin
 #can input multiple regions in the regionlist.txt file to query mutiple regions
 #region=~/ewagner/sratoolkit.2.7.0-centos_linux64/HACKATHON2016/regionlist.txt
 
