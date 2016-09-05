c2f38a93c349f4898433f403e0e570db478d3bc6
Erin Wagner <erinkwagner@users.noreply.github.com>
2016-09-05 17:13:14 -0400

    Update sra_query_tool.sh
    fixed typo

--- bin/sra_query_tool.sh
+++ bin/sra_query_tool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 base= ./sratoolkit.2.7.0-centos_linux64 #path to the sra toolkit folder <default assumes current folder>
 #path to input file with list of SRR IDs
-input= ./SRRlist.txt #add the inout path here <default is the current folder>
+input= ./SRRlist.txt #add the input path here <default is the current folder>
 #path to output folder
 results= ./output #add the output path here <default is the current folder>
 
